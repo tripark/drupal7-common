0d6953a6d9e675a071f242d036fb0121e44881eb
Tripark WWW <tripark@tripark-1.colo.iphouse.net>
2011-06-20 19:16:32 -0500

    added sites/all/libraries

---
+++ sites/all/libraries/mpdf/ttfontdata/dejavusanscondensedI.mtx.php
@@ -0,0 +1,24 @@
+<?php
+$name='DejaVuSansCondensedOblique';
+$type='TTF';
+$desc=array (
+  'Ascent' => 928,
+  'Descent' => -236,
+  'CapHeight' => 928,
+  'Flags' => 68,
+  'FontBBox' => '[-914 -350 1493 1068]',
+  'ItalicAngle' => -11,
+  'StemV' => 87,
+  'MissingWidth' => 540,
+);
+$up=-63;
+$ut=44;
+$ttffile='/home/tripark/drupal6-common/sites/all/libraries/mpdf/ttfonts/DejaVuSansCondensed-Oblique.ttf';
+$TTCfontID='0';
+$originalsize=489032;
+$sip=false;
+$smp=false;
+$BMPselected=true;
+$fontkey='dejavusanscondensedI';
+$panose='0 0 2 11 6 6 3 3 4 11 2 4';
+?>
