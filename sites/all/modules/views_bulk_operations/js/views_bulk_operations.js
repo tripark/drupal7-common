2361d5c1608e30349d8dac94e1789a1da04facc7
Tripark WWW <tripark@tripark-1.colo.iphouse.net>
2011-09-06 11:32:08 -0500

    omnibus update: update Drupal to 7.8 various contrib moudles

--- sites/all/modules/views_bulk_operations/js/views_bulk_operations.js
+++ sites/all/modules/views_bulk_operations/js/views_bulk_operations.js
@@ -1,51 +1,54 @@
 (function ($) {
   Drupal.behaviors.vbo = {
     attach: function(context) {
-      // Add the table behavior for each VBO form found on this page.
       $('.vbo-views-form', context).each(function() {
-        // Table-based styles are special and have their own behaviors.
-        if ($('.views-table', this).length) {
-          Drupal.vbo.initTableBehaviors(this);
-        }
-        else {
-          Drupal.vbo.initGenericBehaviors(this);
-        }
+        Drupal.vbo.initTableBehaviors(this);
+        Drupal.vbo.initGenericBehaviors(this);
       });
     }
   }
 
   Drupal.vbo = Drupal.vbo || {};
   Drupal.vbo.initTableBehaviors = function(form) {
-    // Move the select all markup from the hidden div to a table row.
-    if ($('.vbo-select-all-markup', form).length) {
-      $('tbody', form).prepend('<tr class="views-row-select-all even">></tr>');
+    // If the table is not grouped, "Select all on this page / all pages"
+    // markup gets inserted below the table header.
+    var selectAllMarkup = $('.vbo-table-select-all-markup', form);
+    if (selectAllMarkup.length) {
+      $('tbody', form).prepend('<tr class="views-table-row-select-all even">></tr>');
       var colspan = $('table th', form).length;
-      $('.views-row-select-all', form).html('<td colspan="' + colspan + '">' + $('.vbo-select-all-markup', form).html() + '</td>');
+      $('.views-table-row-select-all', form).html('<td colspan="' + colspan + '">' + selectAllMarkup.html() + '</td>');
+
+      $('.vbo-table-select-all-pages', form).click(function() {
+        Drupal.vbo.tableSelectAllPages(form);
+        return false;
+      });
+      $('.vbo-table-select-this-page', form).click(function() {
+        Drupal.vbo.tableSelectThisPage(form);
+        return false;
+      });
     }
 
-    $('.vbo-select-all', form).show();
-    // Set up the "select all" functionality.
-    $('.vbo-select-all', form).click(function() {
-      $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
-      $('.views-row-select-all', form).toggle();
-    });
-    $('.vbo-select-all-pages', form).click(function() {
-      $('.vbo-this-page', form).hide();
-      $('.vbo-all-pages', form).show();
-      $('.select-all-rows', form).val('1');
-      return false;
-    });
-    $('.vbo-select-this-page', form).click(function() {
-      $('.vbo-all-pages', form).hide();
-      $('.vbo-this-page', form).show();
-      $('.select-all-rows', form).val('0');
-      return false;
+    $('.vbo-table-select-all', form).show();
+    // This is the "select all" checkbox in (each) table header.
+    $('.vbo-table-select-all', form).click(function() {
+      var table = $(this).closest('table')[0];
+      $('input[id^="edit-views-bulk-operations"]:not(:disabled)', table).attr('checked', this.checked);
+
+      // Toggle the visibility of the "select all" row (if any).
+      if (this.checked) {
+        $('.views-table-row-select-all', table).show();
+      }
+      else {
+        $('.views-table-row-select-all', table).hide();
+        // Disable "select all across pages".
+        Drupal.vbo.tableSelectThisPage(form);
+      }
     });
 
     // Set up the ability to click anywhere on the row to select it.
-    $('tbody tr', form).click(function(event) {
+    $('.views-table tbody tr', form).click(function(event) {
       if (event.target.tagName.toLowerCase() != 'input' && event.target.tagName.toLowerCase() != 'a') {
-        $('input[id^="edit-views-bulk-operations"]', this).each(function() {
+        $('input[id^="edit-views-bulk-operations"]:not(:disabled)', this).each(function() {
           var checked = this.checked;
           // trigger() toggles the checkmark *after* the event is set,
           // whereas manually clicking the checkbox toggles it *beforehand*.
@@ -60,6 +63,19 @@
     });
   }
 
+  Drupal.vbo.tableSelectAllPages = function(form) {
+    $('.vbo-table-this-page', form).hide();
+    $('.vbo-table-all-pages', form).show();
+    // Modify the value of the hidden form field.
+    $('.select-all-rows', form).val('1');
+  }
+  Drupal.vbo.tableSelectThisPage = function(form) {
+    $('.vbo-table-all-pages', form).hide();
+    $('.vbo-table-this-page', form).show();
+    // Modify the value of the hidden form field.
+    $('.select-all-rows', form).val('0');
+  }
+
   Drupal.vbo.initGenericBehaviors = function(form) {
     // Show the "select all" fieldset.
     $('.vbo-select-all-markup', form).show();
@@ -67,15 +83,41 @@
     $('.vbo-select-this-page', form).click(function() {
       $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
       $('.vbo-select-all-pages', form).attr('checked', false);
+
+      // Toggle the "select all" checkbox in grouped tables (if any).
+      $('.vbo-table-select-all', form).attr('checked', this.checked);
     });
     $('.vbo-select-all-pages', form).click(function() {
       $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
-      if (this.checked) {
+      $('.vbo-select-this-page', form).attr('checked', false);
+
+      // Toggle the "select all" checkbox in grouped tables (if any).
+      $('.vbo-table-select-all', form).attr('checked', this.checked);
+
+      // Modify the value of the hidden form field.
+      $('.select-all-rows', form).val(this.checked);
+    });
+
+    $('.vbo-select', form).click(function() {
+      // If a checkbox was deselected, uncheck any "select all" checkboxes.
+      if (!this.checked) {
         $('.vbo-select-this-page', form).attr('checked', false);
-        $('.select-all-rows', form).val('1');
-      }
-      else {
-        $('.select-all-rows', form).val('0');
+        $('.vbo-select-all-pages', form).attr('checked', false);
+        // Modify the value of the hidden form field.
+        $('.select-all-rows', form).val('0')
+
+        var table = $(this).closest('table')[0];
+        if (table) {
+          // Uncheck the "select all" checkbox in the table header.
+          $('.vbo-table-select-all', table).attr('checked', false);
+
+          // If there's a "select all" row, hide it.
+          if ($('.vbo-table-select-this-page', table).length) {
+            $('.views-table-row-select-all', table).hide();
+            // Disable "select all across pages".
+            Drupal.vbo.tableSelectThisPage(form);
+          }
+        }
       }
     });
   }
--- sites/all/themes/tpcwf/template.php
+++ sites/all/themes/tpcwf/template.php
@@ -1,1 +1,6 @@
 <?php
+
+
+function tpcwf_preprocess_node(&$variables, $hook) {
+	 $variables['unpublished'] = (!$variables['status']) ? TRUE : FALSE;
+}
